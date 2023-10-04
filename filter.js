0867974db199b7bf83cfebb6e8ffd11ecc3e53a2
Nunna Sujitha <sujithanunna02@gmail.com>
2023-10-04 10:04:55 +0530

    first commit

---
+++ filter.js
@@ -0,0 +1,10 @@
+var arr =[45, 76, 32, 12, 98, 67, 33, 69];
+newArray=arr.filter(function(element,index){
+
+
+        return element > 50 && element <  70;
+    });
+
+
+
+    console.log(newArray);
